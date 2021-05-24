068aac8b994a1f98733c1eeffa3196c681d602a3
sumibhatta <sumibhatta37@gmail.com>
2021-05-24 16:24:34 +0545

    fix: image path

--- js/start.js
+++ js/start.js
@@ -3,34 +3,34 @@ ctx = canvas.getContext('2d')
 
 //load assets
 background = new Image();
-background.src = "/assets/background-night.png";
+background.src = "./assets/background-night.png";
 
 
 block = new Image();
-block.src = "/assets/base.png";
+block.src = "./assets/base.png";
 
 startgame = new Image();
-startgame.src = "/assets/message.png";
+startgame.src = "./assets/message.png";
 
 flappy1 = new Image();
-flappy1.src = "/assets/flappy1.png";
+flappy1.src = "./assets/flappy1.png";
 
 flappy2 = new Image();
-flappy2.src = "/assets/flappy2.png";
+flappy2.src = "./assets/flappy2.png";
 
 flappy3 = new Image();
-flappy3.src = "/assets/flappy3.png";
+flappy3.src = "./assets/flappy3.png";
 
 pipeup = new Image();
-pipeup.src = "/assets/pipe-red.png";
+pipeup.src = "./assets/pipe-red.png";
 
 pipedown = new Image();
-pipedown.src = "/assets/pipe-redd.png";
+pipedown.src = "./assets/pipe-redd.png";
 
 
 gameover = new Image();
 // gameover.src = "../assets/gameover.png";
-gameover.src = '/assets/gameover.png';
+gameover.src = './assets/gameover.png';
 
 let frame= 0;
 let score = 0;
